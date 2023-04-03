f6b561a4b2e41d874b26d1f0d67c8bcd00ba5af3
angelillis.artem <angelillis.artem@studenti.salesianisesto.it>
2023-04-03 11:40:52 +0200

    Update 1.0

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,29 @@
+import java.io.*;
+import java.util.*;
+
 public class Main {
+    public static Scanner in;
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        in = new Scanner(System.in);
+
+        File file;
+
+        if (args.length == 1) {
+            file = new File(args[0]);
+        } else {
+            System.out.print("\nInserisci il Path: ");
+            file = new File(in.next());
+        }
+
+        if (file.isDirectory()) {
+            for (File elem : Objects.requireNonNull(file.listFiles()))
+                if (elem.isFile()) {
+                    System.out.print("\nF " + elem.getName());
+                } else {
+                    System.out.print("\nD " + elem.getName());
+                }
+        } else {
+            System.out.print("\nPath non esistente!");
+        }
     }
 }
